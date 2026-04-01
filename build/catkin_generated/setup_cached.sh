62dd6ea2dd41884c245fff0337566483d1eac6a9
Dhruva Bhardwaj <masterdhruv8@gmail.com>
2026-04-01 15:04:17 +0530

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/prabhav/ssd_2tb/dhruv/DP_run_catkin/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda-12.4/lib64'
+export PATH='/opt/ros/noetic/bin:/home/prabhav/.local/bin:/home/prabhav/miniconda3/condabin:/usr/local/cuda-12.4/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/media/prabhav/SATA_SSD/dhruv/DP_run_catkin/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/prabhav/ssd_2tb/dhruv/DP_run_catkin/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/prabhav/ssd_2tb/dhruv/DP_run_catkin/src:$ROS_PACKAGE_PATH"
